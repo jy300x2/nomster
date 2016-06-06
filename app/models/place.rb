2256a4cfa5a52eaaa8d910aa15725381c001d7f7
Jon Yu <jy300x2@gmail.com>
2016-06-06 09:03:44 +0000

    Added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,7 @@ class Place < ActiveRecord::Base
   validates :name, length: { minimum: 3 }, presence: true
   validates :address, presence: true
   validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
 
