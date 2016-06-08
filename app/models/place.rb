15492ce946061271bd8194d2370f6c6e0a4b4d71
Jon Yu <jy300x2@gmail.com>
2016-06-08 07:12:42 +0000

    Added comments to app

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+  
   validates :name, length: { minimum: 3 }, presence: true
   validates :address, presence: true
   validates :description, presence: true
   geocoded_by :address
   after_validation :geocode
+
 end
 
