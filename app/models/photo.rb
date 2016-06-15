9fb75ce7157fd9bc8ca7a2de8de2abb168512401
Jon Yu <jy300x2@gmail.com>
2016-06-15 06:20:01 +0000

    Modified model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
-  belongs_to :place
-  belongs_to :user
+  belongs_to :place, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 
   mount_uploader :picture, PictureUploader
 end
