4aaa9e7f1cfd2ef6517f3b8165d15dee249c6550
Jon Yu <jy300x2@gmail.com>
2016-06-19 17:26:55 +0000

    Modified model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :photos
+  has_many :comments, dependent: :destroy
+  has_many :photos, dependent: :destroy
   
   validates :name, length: { minimum: 3 }, presence: true
   validates :address, presence: true
