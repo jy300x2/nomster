a224f099a73a4ba462a1a8edad92474c6f6dfdfc
Jon Yu <jy300x2@gmail.com>
2016-06-19 17:40:44 +0000

    Fixed model issues

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :photos, dependent: :destroy
+  has_many :comments, dependent :destroy
+  has_many :photos, dependent :destroy
   
   validates :name, length: { minimum: 3 }, presence: true
   validates :address, presence: true
